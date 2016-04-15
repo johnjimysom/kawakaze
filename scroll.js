e106461af74266c068b04c99463086a1823ce1b4
Johnjimy Som <johnjimy.som@student.csulb.edu>
2016-04-14 23:56:44 -0700

    If you click a button it slowly scrolls to div

---
+++ scroll.js
@@ -0,0 +1,11 @@
+$("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".about").offset().top},
+        'slow');
+});
+
+
+function showDiv() {
+   document.getElementById('welcomeDiv').style.display = "block";
+}
+
